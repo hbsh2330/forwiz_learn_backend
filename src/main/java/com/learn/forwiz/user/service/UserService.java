edc4de1da8f7fa401fc26d58a4651f3017af53b5
jychan99 <108382087+jychan99@users.noreply.github.com>
2025-02-12 17:58:38 +0900

    [정영찬] user테스트데이터 생성 로직
    user테스트데이터 생성 로직

---
+++ src/main/java/com/learn/forwiz/user/service/UserService.java
@@ -0,0 +1,7 @@
+package com.learn.forwiz.user.service;
+
+import com.learn.forwiz.user.vo.UserVO;
+
+public interface UserService {
+	String createUser(UserVO param);
+}
---
+++ src/main/java/com/learn/forwiz/user/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.learn.forwiz.user.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learn.forwiz.user.mapper.UserMapper;
+import com.learn.forwiz.user.service.UserService;
+import com.learn.forwiz.user.vo.UserVO;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserMapper userMapper;
+
+	@Override
+	public String createUser(UserVO param) {
+		return userMapper.createUser(param);
+	}
+}
