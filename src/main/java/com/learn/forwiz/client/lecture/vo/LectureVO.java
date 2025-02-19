63c7439ef10fdd3321661fbcf456d9bf5699eb39
hyeonjun <hjkim@forwiz.com>
2025-02-19 14:48:50 +0900

    [김현준] 강의 썸네일 이미지 보기(임시)

--- src/main/java/com/learn/forwiz/client/lecture/vo/LectureVO.java
+++ src/main/java/com/learn/forwiz/client/lecture/vo/LectureVO.java
@@ -4,31 +4,31 @@ import lombok.Data;
 
 @Data
 public class LectureVO {
-	String lectureNo;
-	String lectureNm;
-	String categoryCd;
-	String price;
-	String detailInfo;
-	String thumbnailImage;
-	String difficulty;
-	String totalCnt;
-	String totalTime;
-	String openYn;
-	String deleteYn;
-	String createdDt;
-	String createdUser;
-	String updatedDt;
-	String updatedUser;
+	String lectureNo;			/* 강의번호     */
+	String lectureNm;			/* 강의명       */
+	String categoryCd;			/* 카테고리코드 */
+	String price;				/* 가격         */
+	String detailInfo;			/* 상세정보     */
+	String thumbnailImage;		/* 썸네일이미지 */
+	String difficulty;			/* 난이도       */
+	String totalCnt;			/* 총강의수     */
+	String totalTime;			/* 총강의시간   */
+	String openYn;				/* 공개여부     */
+	String deleteYn;			/* 삭제여부     */
+	String createdDt;			/* 등록일시     */
+	String createdUser;			/* 등록자       */
+	String updatedDt;			/* 수정일시     */
+	String updatedUser;			/* 수정자       */
 	
 	// 페이징
-	int totalRecordCnt;
-	int page;
-	int pageCnt = 5;
-	int recordCnt = 12;
+	int totalRecordCnt;			/* 총결과개수   */
+	int page;					/* 현재페이지   */
+	int pageCnt = 5;			/* 페이징개수   */
+	int recordCnt = 12;			/* 레코드개수   */
 	
 	// 검색 파라미터
-	String[] difficultyArray;
-	int minPrice;
-	int maxPrice;
-	String orderBy;
+	String[] difficultyArray;	/* 난이도배열   */
+	int minPrice;				/* 최대가격     */
+	int maxPrice;				/* 최소가격     */
+	String orderBy;				/* 정렬         */
 }
--- src/main/java/com/learn/forwiz/config/WebConfig.java
+++ src/main/java/com/learn/forwiz/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.learn.forwiz.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,11 +10,16 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-		        .allowedOrigins("http://localhost:8080", "http://localhost:8081")
-		        .allowCredentials(true)
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:8080", "http://localhost:8081")
+				.allowCredentials(true)
 				.allowedMethods("GET", "POST")
-                .allowedHeaders("*");
-
+				.allowedHeaders("*");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/thumbnail_images/**") 		// URL 패턴
+				.addResourceLocations("file:c:/thumbnail_images/"); // 로컬 폴더 경로
 	}
 }
